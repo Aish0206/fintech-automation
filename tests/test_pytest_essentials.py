75603b4c0942c4ab1e21bb9cdda79dbd8145286d
Aish0206 <gattaniaishwary007@gmail.com>
2026-07-08 00:23:31 +0530

    Learning Task 7-(B): conftest.py

--- tests/test_pytest_essentials.py
+++ tests/test_pytest_essentials.py
@@ -1,22 +1,6 @@
 import pytest
 from practice.account import Account, SavingsAccount, CurrentsAccount
 
-@pytest.fixture(scope="function")
-def normal_account():
-    print ("\t --> In Function Scope - Fixture!!!")
-    return Account("A", 1000)
-
-@pytest.fixture(scope="class")
-def saving_account():
-    print("\t --> In Class Scope - Fixture!!!")
-    yield SavingsAccount("A",1000,5)
-
-@pytest.fixture(scope="module")
-def current_accout():
-    print("\t --> In Module Scope - Fixture!!!")
-    yield CurrentsAccount("A",1000,1000)
-    print("\nTEARDOWN: cleaning up")
-
 class TestAccount:
     
     def test_account_details(self, normal_account):
