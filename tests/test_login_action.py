b33fcfc6ceb26376bd62b566caafbb44c6bbdd01
Aish0206 <gattaniaishwary007@gmail.com>
2026-07-28 01:03:55 +0530

    Task 11: Playwright actions - automated login helper, positive and negative login tests

---
+++ tests/test_login_action.py
@@ -0,0 +1,24 @@
+from playwright.sync_api import Page, expect
+
+LOGIN_PAGE = "https://parabank.parasoft.com/parabank/index.htm"
+
+#Default login Actions
+def login(page: Page, username: str, password: str):
+    page.goto(LOGIN_PAGE)
+    page.locator('input[name="username"]').fill(username)
+    page.locator('input[name="password"]').fill(password)
+    page.get_by_role("button", name="Log In").click()
+
+
+## Positive Scenario
+def test_successful_login(page: Page):
+    login(page,"aish@123","pass@123")
+
+    expect(page.get_by_role("heading",name="Accounts Overview")).to_be_visible()
+
+
+## Negative Scenario
+def test_failed_login(page: Page):
+    login(page,"wrong_user","wrong_pass")
+
+    expect(page.get_by_text("The username and password could not be verified.")).to_be_visible()
